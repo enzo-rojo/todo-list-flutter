525f778afed915de88bc6429b8770abac7d60ccc
enzo-rs <enzorojoall@gmail.com>
2022-08-01 17:08:53 +0200

    add : list model

---
+++ lib/models/list_model.dart
@@ -0,0 +1,11 @@
+import 'package:todo_list_flutter/models/item_list_model.dart';
+
+class ListModel {
+  String name;
+  List<ItemListModel> itemList;
+
+  ListModel({
+    required this.name,
+    required this.itemList,
+  });
+}
