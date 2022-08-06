855500ae83e529eda5a9c417c5fd1f68c6ecde0f
enzo-rs <enzorojoall@gmail.com>
2022-08-06 21:26:03 +0200

    add : firebase connection

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:todo_list_flutter/ui/home/home.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.web,
+  );
 
   runApp(MyApp());
 }
