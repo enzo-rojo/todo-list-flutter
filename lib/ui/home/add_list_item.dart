41c1583d6595f005b301da99ba047bb8f157c084
enzo-rs <enzorojoall@gmail.com>
2022-08-07 17:38:07 +0200

    add : create list item

--- lib/ui/home/add_list_item.dart
+++ lib/ui/home/add_list_item.dart
@@ -1,18 +1,39 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:todo_list_flutter/models/item_list_model.dart';
 
 class AddListItem extends StatefulWidget {
-  AddListItem({Key? key}) : super(key: key);
+  final String listId;
+  final String? userId;
+  AddListItem({
+    Key? key,
+    required this.listId,
+    required this.userId,
+  }) : super(key: key);
 
   @override
   State<AddListItem> createState() => _AddListItemState();
 }
 
 class _AddListItemState extends State<AddListItem> {
+  Future createListItem() async {
+    final docUser = FirebaseFirestore.instance
+        .collection('users/${widget.userId}/lists')
+        .doc(widget.listId);
+
+    final item = ItemListModel(title: "", isChecked: false);
+
+    // Update
+    docUser.update({
+      'itemList': FieldValue.arrayUnion([item.toJson()]),
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return TextButton.icon(
-      onPressed: () => print('pressed'),
+      onPressed: () => createListItem(),
       icon: const Icon(
         Icons.add_rounded,
         size: 22,
